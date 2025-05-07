405e49965e07f5a334fa2df44b601602dcb83919
hi-anki <managemee@protonmail.com>
2025-05-07 06:12:00 +0530

    A simple program to streamline the assembling, linking and running process

---
+++ assemble.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <source.asm>"
+  echo "Usage: $0 <source.s>"
+fi
+
+full_filename=$1
+if [[ "$full_filename" != *.asm && "$full_filename" != *.s ]]; then
+  echo "Invalid file type."
+  exit ;
+fi
+
+filename=${full_filename%%.*}
+
+as "$full_filename" -o "$filename.o" || { echo "Assembling failed"; exit 1; }
+ld "$filename.o" -o "$filename" || { echo "Linking failed"; exit 1; }
+
+./"$filename"
