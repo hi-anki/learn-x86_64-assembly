a33b98c84d0ae4afdd74da02f3e317673456336e
hi-anki <managemee@protonmail.com>
2025-05-09 13:34:01 +0530

    added comments

--- 3-user-input/user-input.asm
+++ 3-user-input/user-input.asm
@@ -7,18 +7,21 @@
   .global _start
 
   _start:
-    mov rax, 0          # sys_read
-    mov rdi, 0          # stdin
-    lea rsi, buffer     # buffer to read into
-    mov rdx, 100        # bytes to read
+  # Step 1: Take user-input
+    mov rax, 0                  # sys_read
+    mov rdi, 0                  # stdin
+    lea rsi, buffer             # buffer to read into
+    mov rdx, 100                # bytes to read
     syscall
 
-    mov rdx, rax        # number of bytes read (from previous syscall)
-    mov rax, 1          # sys_write
-    mov rdi, 1          # stdout
-    lea rsi, buffer     # buffer to write from
+  # Step 2: Display the input
+    mov rdx, rax                # number of bytes read (from previous syscall)
+    mov rax, 1                  # sys_write
+    mov rdi, 1                  # stdout
+    lea rsi, buffer             # buffer to write from
     syscall
 
+  # Exit
     mov rax, 60         # sys_exit
     xor rdi, rdi        # status 0
     syscall
