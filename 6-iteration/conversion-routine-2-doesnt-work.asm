ecc9ad9d654dc9423ab0c7e4de7ea91c843f5010
hi-anki <managemee@protonmail.com>
2025-05-17 17:59:41 +0530

    integer to ascii routine that doesn't work

---
+++ 6-iteration/conversion-routine-2-doesnt-work.asm
@@ -0,0 +1,41 @@
+.intel_syntax noprefix
+
+.section .bss
+  result_buffer: .skip 1
+
+.section .text
+  .global _start
+
+  _start:
+    mov rax, 123
+    lea rdi, [result_buffer + 4]
+    mov byte ptr [rdi], 0
+    dec rdi
+
+  convert_loop:
+    xor rdx, rdx
+    mov rcx, 10
+    div rcx 
+
+    add dl, '0' 
+    mov [rdi], dl
+    dec rdi
+
+    test rax, rax
+    jne convert_loop
+
+    inc rdi
+    jne done
+
+  done:
+    mov rsi, rdi
+    mov rdx, result_buffer + 4
+
+    mov rax, 1
+    mov rdi, 1
+    syscall
+
+  exit:
+    mov rax, 60
+    xor rdi, rdi
+    syscall
