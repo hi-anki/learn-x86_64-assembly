3a61dcd6f12c4a3683cbc18418a89b084a98f8f5
hi-anki <managemee@protonmail.com>
2025-05-27 13:02:37 +0530

    restructured the code to make it suitable for writing my understanding about stack

--- 8-functions/square.asm
+++ 8-functions/square.asm
@@ -3,13 +3,6 @@
 .section .text
 .global _start
 
-_start:
-  mov rdi, 15         # argument: n = 15
-  call square         # call the procedure
-  mov rdi, rax        # move result to rdi for exit
-  mov rax, 60         # syscall: exit
-  syscall
-
 square:
   push rbp            # prologue start, save old base pointer
   mov rbp, rsp        # establish new base pointer, prologue end
@@ -18,3 +11,10 @@ square:
   imul rax, rdi       # rax = rdi * rdi
   pop rbp             # epilogue
   ret                 # return rax, the default place for for return
+
+_start:
+  mov rdi, 15         # argument: n = 15
+  call square         # call the procedure
+  mov rdi, rax        # move result to rdi for exit
+  mov rax, 60         # syscall: exit
+  syscall
